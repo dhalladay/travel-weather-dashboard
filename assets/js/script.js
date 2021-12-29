c4c6c09fc5293e08273d4dd77421cd65967a8c5c
dhalladay <dave.halladay@outlook.com>
2021-12-28 19:12:19 -0700

    created displayCurrentWeather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,59 @@
 var cityButtonsEl = document.querySelector("#search-btn");
 var cityInputEl = document.querySelector("#city-name");
 var formInputEl = document.querySelector("#search-box");
+var currentInputEL = document.querySelector("#current");
+var forecastInputEl = document.querySelector("#forecast")
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
-  var cityName = cityInputEl.value.trim();
+  var city = cityInputEl.value.trim();
 
-  if (cityName) {
-    // getUserRepos(username);
-    // nameInputEl.value="";
-    console.log(cityName);
+  if (city) {
+    convertCity(city);
   } else {
-    alert("Please enter a Github username");
+    alert("Please enter a City");
   }
-  console.log("hello", event);
 };
 
-// formInputEl.addEventListener("submit", formSubmitHandler);
+formInputEl.addEventListener("submit", formSubmitHandler);
 
 var convertCity = function(city) {
-  var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=9c2bde727734176187ec259dc26ddab0";
+  var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=9c2bde727734176187ec259dc26ddab0`;
   fetch(apiURL)
   .then(function(response) {
     if(response.ok) {
       response.json().then(function(data) {
-        console.log(data);
         var locArr = [data[0].lat, data[0].lon];
-        console.log(locArr);
-        oneCall(locArr);
+        oneCall(city, locArr);
       });
     } else {
-      alert("Error: City not found. Try adding a comma and the state or country");
+      alert("Error: City not found. Try adding a comma and the state or country code");
     }
   })
   .catch(function(error) {
-    alert("Unable to connect")
+    alert("Unable to connect to Open Weather Map. Please try again later.")
   });
 };
 
-convertCity();
-
-var oneCall = function(locArr) {
+var oneCall = function(city, locArr) {
   var lat = locArr[0];
   var lon = locArr[1];
-  console.log(lat,lon)
-  var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=9c2bde727734176187ec259dc26ddab0";
+  var apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=9c2bde727734176187ec259dc26ddab0`;
   fetch(apiURL)
   .then(function(response) {
     if(response.ok) {
       response.json().then(function(data) {
-        console.log(data)
+        currentData = data.current;
+        forecastData = data.daily;
+        console.log(data, data.current, data.daily);
       })
+    } else {
+      alert("Unable to find weather data, please try again.");
     }
   })
+  .catch("Unable to connect to Open Weather Map. Please try again later.")
 };
+
+var displayCurrentWeather = function(currentData) {
+  var currentH2 = document.createElement("h2")
+}
